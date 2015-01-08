a22fa3f87670cea8c8b7bed25a4cb2c8c9d26429
kaitlynesparks <kaitlynsparks@Kaitlyns-MacBook-Pro.local>
2015-01-08 13:29:39 -0600

    Accelerate and DistractionFreeCoding Pages

--- dist/app.js
+++ dist/app.js
@@ -147,7 +147,7 @@ angular.module("../app/templates/nav.html", []).run(["$templateCache", function(
     "      <span class=\"icon-bar\"></span>\n" +
     "      <span class=\"icon-bar\"></span>\n" +
     "    </button>\n" +
-    "    <a class=\"navbar-brand\" ui-sref=\"home\">Kaitly Sparks</a>\n" +
+    "    <a class=\"navbar-brand\" ui-sref=\"home\">Kaitlyn Sparks</a>\n" +
     "  </div>\n" +
     "  <div class=\"navbar-collapse collapse navbar-responsive-collapse\">\n" +
     "    <ul class=\"nav navbar-nav navbar-right\">\n" +
@@ -239,7 +239,9 @@ angular.module("../app/views/accelerate.html", []).run(["$templateCache", functi
     "		<br/><br/>\n" +
     "		<strong>Team Members:</strong> Jay Lui, Nicole Navolio, Leticia Patricio\n" +
     "		<br/><br/>\n" +
-    "		<strong>Duration:</strong> October 2014 - December 2014</p>\n" +
+    "		<strong>Duration:</strong> October 2014 - December 2014\n" +
+    "		<br/><br/>\n" +
+    "		<strong>Class:</strong> Interaction Design Fundamentals, CMU</p>\n" +
     "	</div>\n" +
     "</div>\n" +
     "\n" +
@@ -260,7 +262,110 @@ angular.module("../app/views/accelerate.html", []).run(["$templateCache", functi
     "		</div>	\n" +
     "	</div>	\n" +
     "</div>\n" +
-    "<img src=\"../images/Accelerate/Accelerate Final Designs.png\" style=\"width: 800px;\">\n" +
+    "<hr/>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-12\">\n" +
+    "		<h3>Process</h3>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-12\">\n" +
+    "		<div class=\"panel panel-default\">\n" +
+    "			<div class=\"panel-heading\">\n" +
+    "		    	<h3 class=\"panel-title\">Research</h3>\n" +
+    "			</div>\n" +
+    "			<div class=\"panel-body\">\n" +
+    "		    	We started this project by researching the experience and needs of emergency medical staff. We read through internet blogs and articles, watched ambulance ride along videos, and interviewed an EMT.\n" +
+    "		    	<blockquote>\n" +
+    "  					<p>“Myself and many of my co-workers have sought grief counseling, nutrition assistance, and some have even been placed on heavy sleep medications because they never had [targeted information] available to them in a more private medium.”</p>\n" +
+    "  					<small>EMT</small>\n" +
+    "				</blockquote> \n" +
+    "			</div>\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-12\">\n" +
+    "		<div class=\"panel panel-default\">\n" +
+    "			<div class=\"panel-heading\">\n" +
+    "		    	<h3 class=\"panel-title\">Idea Generation</h3>\n" +
+    "			</div>\n" +
+    "			<div class=\"panel-body\">\n" +
+    "		    	We generated concept ideas for apps and created an affinity diagram.  After that, we used sorted them into an feasibility-impact matrix.\n" +
+    "		    	<br/><br/>\n" +
+    "		    	Personas were provided for us.  We used these to determine which ideas to pursue.  We picked the four top choices and composed narratives about their use based on our personas. We used these narratives to make the selection for our final idea, \n" +
+    "		    	<br/><br/>\n" +
+    "				We also followed up with the EMT that we interviewed initially to ask additional questions to make sure our idea was useful and desirable.\n" +
+    "				<br/>\n" +
+    "				<div class=\"row\">\n" +
+    "					<div class=\"col col-md-4\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/Accelerate/ImpactFeasibilityMatrix.jpg\">\n" +
+    "    					</div>\n" +
+    "					</div>\n" +
+    "					<div class=\"col col-md-4\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/Accelerate/AffinityWithPeople.jpg\">\n" +
+    "    					</div>\n" +
+    "					</div>\n" +
+    "					<div class=\"col col-md-4\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/Accelerate/AffinityDiagram.jpg\">\n" +
+    "    					</div>\n" +
+    "					</div>\n" +
+    "				</div>\n" +
+    "			</div>\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-12\">\n" +
+    "		<div class=\"panel panel-default\">\n" +
+    "			<div class=\"panel-heading\">\n" +
+    "				<h3 class=\"panel-title\">Application Design</h3>\n" +
+    "			</div>\n" +
+    "			<div class=\"panel-body\">\n" +
+    "				We fleshed out our idea using a navigation map.  We used this to think through the functionality of our app and the interaction that would be need to move between different screens. We also determined the appropriate depth and breadth of the navigation. We then created sketched wireframes for our main screens and placed them on our navigation map. We continued to update it as we iterated our designs.\n" +
+    "				<br/>\n" +
+    "				<div class=\"row\">\n" +
+    "					<div class=\"col col-md-4\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/Accelerate/NavigationMap.jpg\"></div>\n" +
+    "					</div>\n" +
+    "					<div class=\"col col-md-4\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/Accelerate/NavMapWithSketches.JPG\"></div>\n" +
+    "					</div>	\n" +
+    "					<div class=\"col col-md-4\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/Accelerate/5_NavigationMap_HiFiDesigns.JPG\"></div>\n" +
+    "					</div>\n" +
+    "				</div>  \n" +
+    "				<br/>\n" +
+    "				For iterations, we did sketches, then we did digital low-fidelity mock ups. We went through multiple iterations of high-fidelity mock-ups. We originally had a color scheme and feel that was inspired by ambulances. We had red and blue as our primary colors and had straight lines. However, we later decided that we wanted a more playful and lighthearted theme. EMTs see ambulances all the time and they are likely a stressful place for them many times, so we didn't want to remind them of that. We wanted to give them an application that was enjoyable for them and would help to lift some of their stress.\n" +
+    "				<br/></br/>\n" +
+    "				<div class=\"row\">\n" +
+    "					<div class=\"col col-md-12\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/Accelerate/LowFiMockUps.png\"></div>\n" +
+    "					</div>\n" +
+    "				</div>		\n" +
+    "			</div>\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-12\">\n" +
+    "		<div class=\"panel panel-default\">\n" +
+    "			<div class=\"panel-heading\">\n" +
+    "				<h3 class=\"panel-title\">Final Deliverables</h3>\n" +
+    "			</div>\n" +
+    "			<div class=\"panel-body\">\n" +
+    "				The final deliverables for this project were completed designs, design specifications, and a final presentation.\n" +
+    "				<br/></br/>\n" +
+    "				<div class=\"row\">\n" +
+    "					<div class=\"col col-md-12\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/Accelerate/AccelerateFinalDesignsHorizontal.png\"></div>\n" +
+    "					</div>\n" +
+    "				</div>		\n" +
+    "			</div>\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "\n" +
     "</div>");
 }]);
 
@@ -305,7 +410,144 @@ angular.module("../app/views/contact.html", []).run(["$templateCache", function(
 
 angular.module("../app/views/distractionfreecoding.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("../app/views/distractionfreecoding.html",
-    "<h3>Distraction Free Coding</h3>");
+    "<div class=\"container\">\n" +
+    "<h1>Distraction Free Coding</h1>\n" +
+    "<h4>Contextual Design methodology used to research students working on coding assignments to come up with a vision to solve one of their problems.</h4>\n" +
+    "<hr/>\n" +
+    "\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-4\">\n" +
+    "        <img src=\"../images/DistractionFreeCoding/icon.png\" style=\"width: 300px;\">\n" +
+    "	</div>\n" +
+    "	<div class=\"col col-md-8\">\n" +
+    "		<p><strong>Contributions:</strong> Contextual Inquiry, Affinity Diagrams, Sequence Models, Flow Models, Visioning\n" +
+    "		<br/><br/>\n" +
+    "		<strong>Team Members:</strong> Holly Brosnahan, Amalya Henderson, Michael Richardson, Yueying Tang\n" +
+    "		<br/><br/>\n" +
+    "		<strong>Duration:</strong> October 2014 - December 2014\n" +
+    "		<br/><br/>\n" +
+    "		<strong>Class:</strong> User Centered Research and Evaluation, CMU </p>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "\n" +
+    "<hr/>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-12\">\n" +
+    "		<p><strong>Assignment:</strong> This was a series of assignments designed to practice Contextual Design. The prompt was to observe student developers to see the issues that arise. Teams were then allowed to focus on improving any aspect of the student development process that they observed.\n" +
+    "		<br/><br/>	\n" +
+    "		<strong>Opportunity:</strong> Student developers are easily distracted when trying to do to their homework. They are required to use a lot of systems to perform their tasks, and they are provided with a variety of resources to reference to learn the material. The need to switch between systems coupled with the constant distraction from social media and friends makes it difficult for students to stay focused on their assignments.\n" +
+    "		<br/><br/>\n" +
+    "		<strong>Solution:</strong> A coding environment that blocks messages and provides a single environment for students to reference resources and make annotations. The program locks for a specific period of time for students to work and then provides break time where it allows messages to reach students. It also provides places to take notes that are tied to lines of code and shows resources beside code files which prevents students from having to switch between applications.</p>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "<hr/>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-12\">\n" +
+    "		<h3>Process</h3>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-12\">\n" +
+    "		<div class=\"panel panel-default\">\n" +
+    "			<div class=\"panel-heading\">\n" +
+    "		    	<h3 class=\"panel-title\">Contextual Inquiry</h3>\n" +
+    "			</div>\n" +
+    "			<div class=\"panel-body\">\n" +
+    "		    	We began by performing contextual interviews with developers. Each team member interviewed one student for a total of 5 interviews. During interviews, we went to the location where the developers normally worked and observed them working on their own homework assignments. We took notes to use during interpretation sessions.\n" +
+    "			</div>\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-12\">\n" +
+    "		<div class=\"panel panel-default\">\n" +
+    "			<div class=\"panel-heading\">\n" +
+    "		    	<h3 class=\"panel-title\">Interpretation Sessions</h3>\n" +
+    "			</div>\n" +
+    "			<div class=\"panel-body\">\n" +
+    "		    	During the interpretation sessions, we went through the interviews one at a time.  The interviewer read their notes aloud and the rest of the team worked on creating the flow model and the sequence model. We also created notes about interesting things, breakdowns, and design ideas that would be used later to create the affinity diagram.\n" +
+    "		    	<br/><br/>\n" +
+    "		    	<div class=\"row\">\n" +
+    "					<div class=\"col col-md-6\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/DistractionFreeCoding/SequenceModel.JPG\">\n" +
+    "    					</div>\n" +
+    "					</div>\n" +
+    "					<div class=\"col col-md-6\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/DistractionFreeCoding/FlowModel.JPG\">\n" +
+    "    					</div>\n" +
+    "					</div>\n" +
+    "				</div>\n" +
+    "			</div>\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-12\">\n" +
+    "		<div class=\"panel panel-default\">\n" +
+    "			<div class=\"panel-heading\">\n" +
+    "		    	<h3 class=\"panel-title\">Affinity Diagram and Consolidated Models</h3>\n" +
+    "			</div>\n" +
+    "			<div class=\"panel-body\">\n" +
+    "		    	We took the notes we created during the interpretation session and grouped them to create an affinity diagram. When grouping these notes, we creating three layers of headings. The lower two are in the customer perspective. This allowed us to look back at the affinity diagram later and just reading the headings to get an idea of what was happening. We also consolidated the 5 sequences into one sequence that represented all of the users.  We did the same with the flow models to create one consolidated model.\n" +
+    "		    	<br/><br/>\n" +
+    "		    	<div class=\"row\">\n" +
+    "					<div class=\"col col-md-12\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/DistractionFreeCoding/Affinity.JPG\">\n" +
+    "    					</div>\n" +
+    "					</div>\n" +
+    "				</div>\n" +
+    "				<div class=\"row\">\n" +
+    "					<div class=\"col col-md-6\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/DistractionFreeCoding/ConsolidatedFlow.jpg\">\n" +
+    "    					</div>\n" +
+    "					</div>\n" +
+    "					<div class=\"col col-md-6\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/DistractionFreeCoding/ConsolidatedSequence.JPG\">\n" +
+    "    					</div>\n" +
+    "					</div>\n" +
+    "				</div>\n" +
+    "			</div>\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-12\">\n" +
+    "		<div class=\"panel panel-default\">\n" +
+    "			<div class=\"panel-heading\">\n" +
+    "		    	<h3 class=\"panel-title\">Visioning</h3>\n" +
+    "			</div>\n" +
+    "			<div class=\"panel-body\">\n" +
+    "		    	After consolidating the models, we began visioning. We revisited our models and affinity diagram and generated design ideas and determined the key problems we needed to address. Then we listed the technologies we had available to us to solve the problems. While looking at our models and technologies, we began visioning by sketching out possible scenarios in an improv style session with one person being the writer and the other team members speaking their ideas aloud. \n" +
+    "		    	<br/><br/>\n" +
+    "		    	<div class=\"row\">\n" +
+    "					<div class=\"col col-md-6\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/DistractionFreeCoding/GenerateIdeas.JPG\">\n" +
+    "    					</div>\n" +
+    "					</div>\n" +
+    "					<div class=\"col col-md-6\">\n" +
+    "						<div class=\"thumbnail\"><img src=\"../images/DistractionFreeCoding/Vision.JPG\">\n" +
+    "    					</div>\n" +
+    "					</div>\n" +
+    "				</div>\n" +
+    "			</div>\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "<div class=\"row\">\n" +
+    "	<div class=\"col col-md-12\">\n" +
+    "		<div class=\"panel panel-default\">\n" +
+    "			<div class=\"panel-heading\">\n" +
+    "		    	<h3 class=\"panel-title\">Final Deliverables</h3>\n" +
+    "			</div>\n" +
+    "			<div class=\"panel-body\">\n" +
+    "		    	After we did 5 visions, we determined the ones we thought were most promising based on the information we learned about our users while generating our affinity diagram, sequence models, and flow models.  We then consolidated the visions containing those ideas to create a final idea. As a final deliverable, we presented our findings and our final idea to the class.\n" +
+    "			</div>\n" +
+    "		</div>\n" +
+    "	</div>\n" +
+    "</div>\n" +
+    "\n" +
+    "\n" +
+    "</div>");
 }]);
 
 angular.module("../app/views/hackathon.html", []).run(["$templateCache", function($templateCache) {
